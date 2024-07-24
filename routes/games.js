e6cf9bb9020d1385301c5902d779d9f6415b8ed8
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-07-24 03:04:19 -0500

    Update web sources

--- routes/games.js
+++ routes/games.js
@@ -7,12 +7,12 @@ route.get("/", function (req, res) {
     res.render('serverlist');
 })
 
-// Game: Minecraft 2
-route.get('/mc2', (req, res) => {
-    fetch("https://api.mcstatus.io/v2/status/java/theatrekids.uk")
+// Game: TKM2
+route.get('/tkm2', (req, res) => {
+    fetch("https://api.mcstatus.io/v2/status/java/smp.theatrekids.uk")
     .then(async data => {
         let mc_info = await data.text();
-        res.render('servers/mc2', {
+        res.render('servers/tkm2', {
             minecraft: JSON.parse(mc_info)
         });
     }).catch(err => {
